e9b425bdc968664b540b0e5d26baba68e4e8a737
TranXuanTuan <tranxuantuan2909@gmail.com>
2019-11-22 09:30:13 +0700

    edit db

--- database/migrations/2019_11_21_064837_create_blogs_table.php
+++ database/migrations/2019_11_21_064837_create_blogs_table.php
@@ -15,6 +15,7 @@ class CreateBlogsTable extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
             $table->string('title');
             $table->string('description');
             $table->string('content');
