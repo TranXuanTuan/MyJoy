b0e55e153b3d70434140614770827ed406da5511
TranXuanTuan <tranxuantuan2909@gmail.com>
2019-12-02 16:47:03 +0700

    edit create artist

--- resources/views/admin/artists/create.blade.php
+++ resources/views/admin/artists/create.blade.php
@@ -29,7 +29,7 @@
                         <div class="form-group">
                             <label for="customFile">Avatar</label>
                             <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="customFile" name="image">
+                                <input type="file" class="custom-file-input" id="customFile" name="avatar">
                                 <label class="custom-file-label" for="customFile">Choose file</label>
                             </div>
                         </div>
