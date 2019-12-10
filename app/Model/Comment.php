7d0aaf1a8345181e9be450cfb0f019906f2a4d67
TranXuanTuan <tranxuantuan2909@gmail.com>
2019-12-10 15:41:33 +0700

    fix bugs

--- app/Model/Comment.php
+++ app/Model/Comment.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+	protected $fillable = [
+        'user_id', 'blog_id','comment'
+    ];
     public function user()
     {
     	return $this->belongsTo('App\User');
